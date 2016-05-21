fc989aba93faebb9f0d22aeb435a27ae460e0a61
janetHugarcia <janet.huacahuasi.garcia@gmail.com>
2016-05-21 07:01:14 -0500

    mixitup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+  //-------------------------
+    $(function(){
+      $('#containerin').mixItUp();
+    });
+  //----------------------
 	function Opacidad(){
         if ($('#foco').hasClass('white')) {
              $('#foco').fadeTo('slow',0.3)
@@ -12,65 +17,29 @@ $(document).ready(function(){
 
     setInterval(Opacidad, 1000);
 
-    
-    $(function() {
-      $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
-    });
-
-
-  /*sticky*/
-  // Hide Header on on scroll down
-  var didScroll;
-  var lastScrollTop = 0;
-  var delta = 5;
-  var navbarHeight = $('header').outerHeight();
-
-  $(window).scroll(function(event){
-      didScroll = true;
-  });
-
-  setInterval(function() {
-      if (didScroll) {
-          hasScrolled();
-          didScroll = false;
-      }
-  }, 250);
-
-  function hasScrolled() {
-      var st = $(this).scrollTop();
-      
-      // Make sure they scroll more than delta
-      if(Math.abs(lastScrollTop - st) <= delta)
-          return;
-      
-      // If they scrolled down and are past the navbar, add class .nav-up.
-      // This is necessary so you never see what is "behind" the navbar.
-      if (st > lastScrollTop && st > navbarHeight){
-          // Scroll Down
-          $('header').removeClass('menu-fixed').addClass('nav-up');
-      } else {
-          // Scroll Up
-          if(st + $(window).height() < $(document).height()) {
-              $('header').removeClass('nav-up').addClass('menu-fixed');
-          }
-      }
-      
-      lastScrollTop = st;
-  }
 
   /*How to make the hero section always fill browser window?*/
   $(window).resize(function() {
           $('#hero').height($(window).height());
   }).resize();
+
+
+  $('.nav-link').on('click', function(e) {
+    e.preventDefault();
+    var el = $(this).attr('href');
+    scrollToElement(el);
+  });
+  
+  $(window).scroll(function() {
+    var x = $(window).scrollTop();
+
+    if (x >= 42) {
+      $("#navbar").show(200);
+    } else {
+      $("#navbar").hide(200);
+    }
+
+  });
+
 });
+
