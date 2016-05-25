6a2e684882e3f9ffe18d21f9d777faa3853d14ac
janetHugarcia <janet.huacahuasi.garcia@gmail.com>
2016-05-25 17:08:05 -0500

    agregando lines

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,32 @@ $('a[href^="#"]').on('click', function(event) {
       $('.navbar').removeClass('menu-fixed');
     }
   });
+  //-----------------lines-
+
+
+
+  jQuery.rnd = function(m,n) {
+      m = parseInt(m);
+      n = parseInt(n);
+      return Math.floor( Math.random() * (n - m + 1) ) + m;
+  }
+  function lines() {
+   $.each($(".particletext.lines"), function(){
+      var linecount = ($(this).width()/50)*10;
+      for(var i = 0; i <= linecount; i++) {
+         $(this).append('<span class="particle" style="top:' + $.rnd(-30,30) + '%; left:' + $.rnd(-10,110) + '%;width:' + $.rnd(1,3) + 'px; height:' + $.rnd(20,80) + '%;animation-delay: -' + ($.rnd(0,30)/10) + 's;"></span>');
+      }
+   });
+  }
+  function initparticles() {
+
+   lines();
+
+  }
+  
+
+  initparticles();
+
  
 });
 
